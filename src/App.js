8dca40c5415ff164caf4008bf6470258cae6fc12
Leesa Ward <leesa.ward@nab.com.au>
2021-08-27 20:05:08 +1000

    Set up API query to get definitions when a word is searched for and save them to a state variable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import Search from './components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-
+		<Search/>
     </div>
   );
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import { ThemeProvider } from 'styled-components';
 import { styledHelpers, TextField, Button } from 'grape-ui-react';
 import { SearchForm } from './Search.style';
@@ -17,7 +18,9 @@ export interface SearchProps {
 export const Search: React.FC<SearchProps> = function(props: {
 
 	}) {
-	const [searchTerm, setSearchTerm] = useState('');
+	const [searchTerm, setSearchTerm] = useState<string>('');
+	const apiKeyDict = '16a63a57-7277-4843-8034-4285a3b986ee';
+	const [definitions, setDefinitions] = useState<any[]>([]);
 
 	function updateSearchTerm(event: React.ChangeEvent<HTMLInputElement>) {
 		setSearchTerm(event.target.value);
@@ -25,7 +28,21 @@ export const Search: React.FC<SearchProps> = function(props: {
 
 	function handleSearch(event: { preventDefault: () => void; }) {
 		event.preventDefault();
-		alert('handle search');
+
+		getDefinitions(searchTerm).then(definitions => {
+			setDefinitions(definitions.slice(0,3));
+		})
+	}
+
+	function getDefinitions(term: string) {
+		const query = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=${apiKeyDict}`;
+
+		return axios.get(query)
+			.then(response => {
+				return response.data;
+			}).catch(error => {
+				console.log(error);
+			})
 	}
 
 	return (
